7e13f804af676a5fed1c0bbc359be173ece32d4c
deng <2114832690@qq.com>
2023-11-26 22:57:05 +0800

    修改

--- themes/next/components/SideAreaRight.js
+++ themes/next/components/SideAreaRight.js
@@ -31,8 +31,7 @@ const SideAreaRight = (props) => {
 
         {siteConfig('NEXT_RIGHT_AD', null, CONFIG) && <Card className='mb-2'>
             {/* 展示广告  */}
-            <span>暂无</span>
-            {/* <ins
+            <ins
                 className='adsbygoogle'
                 style={{ display: 'block' }}
                 data-adtest='on'
@@ -40,7 +39,7 @@ const SideAreaRight = (props) => {
                 data-ad-slot='8807314373'
                 data-ad-format='auto'
                 data-full-width-responsive='true'
-            /> */}
+            />
         </Card>}
 
         <div className="sticky top-0 space-y-4 w-full">
